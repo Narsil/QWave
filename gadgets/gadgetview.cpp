c57a6555035393386c9afda520e83979d331aa77
Torben Weis <torben.weis@gmail.com>
2009-11-19 16:03:26 +0000

    Fix for gadgets

--- gadgets/gadgetview.cpp
+++ gadgets/gadgetview.cpp
@@ -7,7 +7,7 @@ GadgetView::GadgetView(const QUrl& url, Environment* environment)
         : QWebView(), m_environment(environment)
 {
     this->setGeometry( 0, 0, 300, 150 );
-    this->setHtml("<html><body style=\"background-color:#888888\"><h1>Loading gadget ...</h1></body></html>");
+    this->setHtml("<html><body style=\"background-color:#888888; overflow:hidden\"><h1>Loading gadget ...</h1></body></html>");
 
     m_api = new GadgetAPI( page()->mainFrame(), this );
     m_manifest = new GadgetManifest( url, m_environment, this );
@@ -18,5 +18,5 @@ GadgetView::GadgetView(const QUrl& url, Environment* environment)
 void GadgetView::manifestFinished()
 {
     QString content = m_manifest->content();
-    this->setHtml("<html><body>" + content.mid(9, content.length() - 12) + "</body></html>");
+    this->setHtml("<html><body style=\"overflow:hidden\">" + content.mid(9, content.length() - 12) + "</body></html>");
 }
