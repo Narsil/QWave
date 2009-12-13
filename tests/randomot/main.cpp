e6be43d54ade3bee8c123a8521317dfb04bb5992
torben.weis <torben.weis@4cf79858-b814-11de-b7b1-c13d6a5ff25b>
2009-12-13 02:22:02 +0000

    Generates random OT mutations for testing
    git-svn-id: https://qwaveclient.googlecode.com/svn/trunk@110 4cf79858-b814-11de-b7b1-c13d6a5ff25b

--- tests/randomot/main.cpp
+++ tests/randomot/main.cpp
@@ -28,6 +28,7 @@
 
 private:
     StructuredDocument* createDocument();
+    DocumentMutation* createMutation(StructuredDocument* doc);
  };
 
 void RandomOT::initTestCase()
@@ -38,10 +39,13 @@ void RandomOT::initTestCase()
 
 void RandomOT::randomOT()
 {
-    for( int i = 0; i < 10; ++i )
+    for( int i = 0; i < 20; ++i )
     {
         StructuredDocument* doc = createDocument();
         doc->print_();
+
+        DocumentMutation* m = createMutation(doc);
+        m->print_();
 
         delete doc;
     }
@@ -51,6 +55,108 @@ void RandomOT::cleanupTestCase()
 {
 }
 
+DocumentMutation* RandomOT::createMutation(StructuredDocument* doc)
+{
+    DocumentMutation* m = new DocumentMutation();
+
+    bool deleting = false;
+    bool stopDeleting = false;
+    int deleteDepth = 0;
+    for( int i = 0; i < doc->count(); ++i )
+    {
+        if ( deleting && qrand() % 3 == 0 )
+        {
+            if ( deleteDepth == 0 )
+            {
+                stopDeleting = false;
+                deleting = false;
+            }
+            else
+               stopDeleting = true;
+        }
+        switch( doc->typeAt(i) )
+        {
+            case StructuredDocument::Start:
+            {
+                int r = qrand();
+                if ( r % 10 <= 1 || deleting )
+                {
+                    deleting = true;
+                    m->deleteStart( doc->tagAt(i), doc->attributesAt(i) );
+                    deleteDepth++;
+                }
+                else if ( r % 10 <= 3 )
+                {
+                    m->retain(1);
+                }
+                else if ( r % 10 <= 6 )
+                {
+                    // Update
+                    StructuredDocument::AttributeList attribs = doc->attributesAt(i);
+                    QHash<QString,StructuredDocument::StringPair> update;
+                    int count = qrand() % 4 + 1;
+                    for( int a = 0; a < count; ++a )
+                    {
+                        QString at( QChar( 'a' + (qrand()%10) ) );
+                        update[at] = StructuredDocument::StringPair(attribs[at], QString( QChar( 'a' + (qrand()%26) ) ) );
+                    }
+                    m->updateAttributes(update);
+                }
+                else
+                {
+                    // Replace
+                    StructuredDocument::AttributeList attribs = doc->attributesAt(i);
+                    StructuredDocument::AttributeList newAttribs;
+                    int count = qrand() % 3 + 1;
+                    for( int i = 0; i < count; ++i )
+                        newAttribs[ QString( QChar( 'a' + (qrand()%10) ) ) ] = QString( QChar( 'a' + (qrand()%26) ) );
+                    attribs.remove("**t");
+                    m->replaceAttributes( attribs, newAttribs );
+                }
+            }
+            break;
+            case StructuredDocument::End:
+                if ( deleting )
+                {
+                    if ( deleteDepth == 0 )
+                    {
+                        m->retain(1);
+                        stopDeleting = false;
+                        deleting = false;
+                    }
+                    else
+                    {
+                        m->deleteEnd();
+                        deleteDepth--;
+                        if ( stopDeleting && deleteDepth == 0 )
+                        {
+                            stopDeleting = false;
+                            deleting = false;
+                        }
+                    }
+                }
+                else
+                    m->retain(1);
+                break;
+            case StructuredDocument::Char:
+            {
+                QString text( doc->charAt(i) );
+                int r = qrand();
+                if ( r % 10 <= 1 || deleting )
+                {
+                    deleting = true;
+                    m->deleteChars(text);
+                }
+                else
+                    m->retain(1);
+            }
+            break;
+        }
+    }
+
+    return m;
+}
+
 StructuredDocument* RandomOT::createDocument()
 {
     StructuredDocument* doc = new StructuredDocument();
@@ -58,7 +164,7 @@ StructuredDocument* RandomOT::createDocument()
     QStack<QString> tags;
     DocumentMutation m;
     int r = 0;
-    while( ( r = qrand() ) >= RAND_MAX / 10 )
+    while( ( r = qrand() ) >= RAND_MAX / 20 )
     {
         if ( r % 10 > 1 )
         {
@@ -72,9 +178,9 @@ StructuredDocument* RandomOT::createDocument()
         else
         {
             StructuredDocument::AttributeList attribs;
-            int count = qrand() % 10;
-            for( int i = 0; i < count - 6; ++i )
-                attribs[ QString( QChar( 'a' + (qrand()%26) ) ) ] = QString( QChar( 'a' + (qrand()%26) ) );
+            int count = qrand() % 9;
+            for( int i = 0; i < count - 5; ++i )
+                attribs[ QString( QChar( 'a' + (qrand()%10) ) ) ] = QString( QChar( 'a' + (qrand()%26) ) );
             QString tag = QString( QChar( 'a' + (qrand()%26) ) );
             m.insertStart( tag, attribs );
             tags.push(tag);
