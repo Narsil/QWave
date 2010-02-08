88d3c2734e293b0087e3fabb6fdb8547dd4c8ec0
torben.weis <torben.weis@4cf79858-b814-11de-b7b1-c13d6a5ff25b>
2010-02-08 17:20:44 +0000

    Code cleanup, comments and fix for the timeout receiver
    git-svn-id: https://qwaveclient.googlecode.com/svn/trunk@194 4cf79858-b814-11de-b7b1-c13d6a5ff25b

--- waveserver/actor/timeout.h
+++ waveserver/actor/timeout.h
@@ -8,6 +8,9 @@
 class QTimer;
 class TimeoutImpl;
 
+/**
+  * @internal
+  */
 class TimeoutMessage : public IMessage
 {
 public:
@@ -15,11 +18,15 @@ public:
     TimeoutImpl* ptr;
 };
 
+/**
+  * @internal
+  */
 class TimeoutImpl : public QObject, public WaitingConditionImpl
 {
     Q_OBJECT
 public:
     TimeoutImpl(int timeout);
+    ~TimeoutImpl();
 
     virtual WaitingConditionImpl* handleMessage( const QSharedPointer<IMessage>& msg )
     {
@@ -33,11 +40,17 @@ private slots:
     void timeout();
 
 private:
-    int m_timeout;
+    QTimer* m_timer;
 };
 
+/**
+  * This class can be used as a parameter to the yield statement inside an Actor.
+  */
 typedef WaitingConditionPointer2<TimeoutImpl, int> Timeout;
 
+/**
+  * Allows to concatenate a timeout with other WaitingCondition classes.
+  */
 template<class T, class P1, class X> RecvXor operator|( const WaitingConditionPointer2<T,P1>& x, const X& y ) { return RecvXor(x.donate(), y.donate()); }
 
 #endif // TIMEOUT_H
