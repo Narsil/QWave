7c3326cb154782563982ad99a09b80c0c0cf5575
torben.weis <torben.weis@4cf79858-b814-11de-b7b1-c13d6a5ff25b>
2010-03-06 15:03:45 +0000

    Started work on OT in native JavaScript
    git-svn-id: https://qwaveclient.googlecode.com/svn/trunk@247 4cf79858-b814-11de-b7b1-c13d6a5ff25b

--- webclient/javascript/jsot.js
+++ webclient/javascript/jsot.js
@@ -8,7 +8,7 @@ JSOT.Doc = function()
 
 JSOT.Doc.ElementStart = function(type, attributes)
 {
-	this.elementStart = true;
+	this.element_start = true;
 	this.type = type;
 	if ( !attributes )
 		this.attributes = { };
@@ -18,7 +18,7 @@ JSOT.Doc.ElementStart = function(type, attributes)
 
 JSOT.Doc.ElementEnd = function()
 {
-	this.elementEnd = true;
+	this.element_end = true;
 };
 
 JSOT.Doc.prototype.toString = function()
@@ -44,7 +44,7 @@ JSOT.Doc.prototype.toString = function()
 				anno = a;
 			}
 		}
-		if ( c.elementStart )
+		if ( c.element_start )
 		{
 			stack.push( c.type );
 			result.push("<" + c.type);
@@ -59,7 +59,7 @@ JSOT.Doc.prototype.toString = function()
 			}
 			result.push(">");
 		}
-		else if ( c.elementEnd )
+		else if ( c.element_end )
 		{
 			result.push("<" + stack.pop() + "/>");
 		}
@@ -100,20 +100,20 @@ JSOT.DocOp.prototype.applyTo = function(doc)
 	while( opIndex < this.component.length )
 	{
 		var op = this.component[opIndex++];
-		if ( op.elementStart )
+		if ( op.element_start )
 		{
 			if ( deleteDepth > 0 )
 				throw "Cannot insert inside a delete sequence";
 
 			if ( inContentIndex == 0 )
 			{
-				doc.content.splice( contentIndex, 0, new JSOT.Doc.ElementStart( op.elementStart.type, op.elementStart.attribute ) );
+				doc.content.splice( contentIndex, 0, new JSOT.Doc.ElementStart( op.element_start.type, op.element_start.attribute ) );
 				doc.format.splice( contentIndex, 0, updatedAnnotation );
 				c = doc.content[++contentIndex];
 			}
 			else
 			{
-				doc.content.splice( contentIndex + 1, 0, new JSOT.Doc.ElementStart( op.elementStart.type, op.elementStart.attribute ), c.substring(inContentIndex, c.length) );
+				doc.content.splice( contentIndex + 1, 0, new JSOT.Doc.ElementStart( op.element_start.type, op.element_start.attribute ), c.substring(inContentIndex, c.length) );
 				doc.format.splice( contentIndex + 1, 0, updatedAnnotation, docAnnotation );
 				doc.content[contentIndex] = c.slice(0, inContentIndex);
 				contentIndex += 2;
@@ -121,7 +121,7 @@ JSOT.DocOp.prototype.applyTo = function(doc)
 				inContentIndex = 0;
 			}			
 		}
-		else if ( op.elementEnd )
+		else if ( op.element_end )
 		{
 			if ( deleteDepth > 0 )
 				throw "Cannot insert inside a delete sequence";
@@ -244,7 +244,7 @@ JSOT.DocOp.prototype.applyTo = function(doc)
 					// Update the annotation
 					doc.format[contentIndex] = updatedAnnotation;
 				}
-				if ( c.elementStart || c.elementEnd )
+				if ( c.element_start || c.element_end )
 				{
 					// Skip the element
 					count--;
@@ -291,7 +291,7 @@ JSOT.DocOp.prototype.applyTo = function(doc)
 			}
 			// Count how many opening elements have been deleted. The corresponding closing elements must be deleted, too.
 			deleteDepth++;
-			if ( !c.elementStart )
+			if ( !c.element_start )
 				throw "Cannot delete element start at this position, because in the document there is none";
 			if ( c.type != op.delete_element_start.type )
 				throw "Cannot delete element start because Op and Document have different element type";
@@ -321,7 +321,7 @@ JSOT.DocOp.prototype.applyTo = function(doc)
 			// Is there a matching openeing element?
 			if ( deleteDepth == 0 )
 				throw "Cannot delete element end, because matching delete element start is missing";
-			if ( !c.elementEnd )
+			if ( !c.element_end )
 				throw "Cannot delete element end at this position, because in the document there is none";
 			doc.content.splice( contentIndex, 1 );
 			doc.format.splice( contentIndex, 1 );
@@ -333,7 +333,7 @@ JSOT.DocOp.prototype.applyTo = function(doc)
 		{		
 			if ( !c )
 				break;
-			if ( !c.elementStart )
+			if ( !c.element_start )
 				throw "Cannot update attributes at this position, because in the document there is no start element";
 			// If there is an annotation boundary change in the deleted characters, this change must be applied
 			var anno = doc.format[contentIndex];
@@ -373,7 +373,7 @@ JSOT.DocOp.prototype.applyTo = function(doc)
 		{
 			if ( !c )
 				break;
-			if ( !c.elementStart )
+			if ( !c.element_start )
 				throw "Cannot replace attributes at this position, because in the document there is no start element";
 			if ( c.attributes.length != op.replace_attributes.oldAttributes.length )
 				throw "Cannot replace attributes because the old attributes do not match";
@@ -478,16 +478,66 @@ JSOT.DocOp.prototype.computeAnnotation = function(docAnnotation, annotationUpdat
 	return anno;
 };
 
+JSOT.DocOp.prototype.has_element_start = function()
+{
+	return this.element_start != null;
+};
+
+JSOT.DocOp.prototype.has_element_end = function()
+{
+	return this.element_end == true;
+};
+
+JSOT.DocOp.prototype.has_delete_element_start = function()
+{
+	return this.delete_element_start != null;
+};
+
+JSOT.DocOp.prototype.has_delete_element_end = function()
+{
+	return this.delete_element_end == true;
+};
+
+JSOT.DocOp.prototype.has_characters = function()
+{
+	return this.characters != null;
+};
+
+JSOT.DocOp.prototype.has_delete_characters = function()
+{
+	return this.delete_characters != null;
+};
+
+JSOT.DocOp.prototype.has_retain_item_count = function()
+{
+	return this.retain_item_count != null;
+};
+
+JSOT.DocOp.prototype.has_replace_attributes = function()
+{
+	return this.replace_attributes != null;
+};
+
+JSOT.DocOp.prototype.has_update_attributes = function()
+{
+	return this.update_attributes != null;
+};
+
+JSOT.DocOp.prototype.has_annotation_boundary = function()
+{
+	return this.annotation_boundary != null;
+};
+
 JSOT.DocOp.newElementStart = function(type, attributes)
 {
 	if ( !attributes )
 		attributes = { };
-	this.elementStart = { type : type, attribute : attributes };
+	this.element_start = { type : type, attribute : attributes };
 };
 
 JSOT.DocOp.newElementEnd = function()
 {
-	this.elementEnd = true;
+	this.element_end = true;
 };
 
 JSOT.DocOp.newCharacters = function(characters)
@@ -547,4 +597,219 @@ JSOT.ProtocolWaveletOperation.MutateDocument = function(documentId, documentOper
 {
 	this.document_id = documentId;
 	this.document_operation = documentOperation;
+};
+
+/////////////////////////////////////////////////
+//
+// OT
+//
+/////////////////////////////////////////////////
+
+JSOT.DocOp.xform = function( m1, m2 )
+{
+    if ( m1.component.length == 0 || m2.component.length == 0 )
+        return;
+
+	var r1, r2, anno1, anno2;
+	var c1 = 0;
+	var c2 = 0;
+	var item1 = m1.component[c1];
+	var item2 = m2.component[c2];
+	var next = { next1 : false, next2 : false };
+    while( c1 < m1.component.length && c2 < m2.component.length )
+    {
+        if ( item1.has_element_start() )
+        {           
+			JSOT.DocOp.xformInsertElementStart( r1, r2, item1, item2, next, anno1, anno2 );
+		}
+		else if ( item1.has_element_end() )
+		{
+			JSOT.DocOp.xformInsertElementEnd( r1, r2, item1, item2, next, anno1, anno2 );
+		}
+		else if ( item1.has_characters() )
+		{
+			JSOT.DocOp.xformInsertChars( r1, r2, item1, item2, next, anno1, anno2 );
+		}
+		else if ( item1.has_retain_item_count() )
+		{
+			JSOT.DocOp.xformRetain( r1, r2, item1, item2, next, anno1, anno2 );
+		}
+		else if ( item1.has_delete_element_start() )
+		{
+			JSOT.DocOp.xformDeleteElementStart( r1, r2, item1, item2, next1, next2, anno1, anno2, ok );
+		}
+		else if ( item1.has_delete_element_end() )
+		{
+			JSOT.DocOp.xformDeleteElementEnd( r1, r2, item1, item2, next1, next2, anno1, anno2, ok );
+		}
+		else if ( item1.has_delete_characters() )
+		{
+			JSOT.DocOp.xformDeleteChars( r1, r2, item1, item2, next1, next2, anno1, anno2, ok );
+		}
+		else if ( item1.has_update_attributes() )
+		{
+			JSOT.DocOp.xformUpdateAttributes( r1, r2, item1, item2, next1, next2, anno1, anno2, ok );
+		}
+		else if ( item1.has_replace_attributes() )
+		{
+			JSOT.DocOp.xformReplaceAttributes( r1, r2, item1, item2, next1, next2, anno1, anno2, ok );
+		}
+		else if ( item1.has_annotation_boundary() )
+		{
+			JSOT.DocOp.xformAnnotationBoundary( r1, r2, item1, item2, next1, next2, anno1, anno2, false, ok );
+        }
+
+        if ( next.next1 )
+        {
+            c1++;
+            if ( c1 < m1.component.length )
+                item1 = m1.component[c1];
+        }
+        if ( next.next2 )
+        {
+            c2++;
+            if ( c2 < m2.component.length )
+                item2 = m2.component[c2];
+        }
+    }
+
+    if ( c1 != m1.component.length || c2 != m2.component.length )    
+        throw "Length mismatch when transforming deltas.";
+
+	m1.component = r1.component;
+	m2.component = r2.component;
+};
+
+JSOT.DocOp.xformInsertElementStart = function( r1, r2, item1, item2, next, anno1, anno2 )
+{
+	r1.component.push( item1 );
+	r2.component.push( JSOT.DocOp.newRetainItemCount( 1 ) );
+    next.next1 = true;
+    next.next2 = false;
+};
+
+JSOT.DocOp.xformInsertElementEnd = function( r1, r2, item1, item2, next, anno1, anno2 )
+{
+	r1.component.push( item1 );
+	r2.component.push( JSOT.DocOp.newRetainItemCount( 1 ) );
+    next.next1 = true;
+    next.next2 = false;
+};
+
+JSOT.DocOp.xformInsertChars = function( r1, r2, item1, item2, next, anno1, anno2 )
+{
+	r1.component.push( item1 );
+	r2.component.push( JSOT.DocOp.newRetainItemCount( item1.characters.length ) );
+    next.next1 = true;
+    next.next2 = false;
+};
+
+JSOT.DocOp.xformRetain = function( r1, r2, item1, item2, next, anno1, anno2 )
+{
+	if ( item2.has_element_start() )
+	{
+		JSOT.DocOp.xformInsertElementStart( r2, r1, item2, item1, next, anno2, anno1);
+	}
+	else if ( item2.has_element_end() )
+	{
+		JSOT.DocOp.xformInsertElementEnd( r2, r1, item2, item1, next, anno2, anno1);
+	}
+	else if ( item2.has_characters() )
+	{
+		JSOT.DocOp.xformInsertChars( r2, r1, item2, item1, next, anno2, anno1);
+	}
+	else if ( item2.has_annotation_boundary() )
+	{
+		JSOT.DocOp.xformAnnotationBoundary( r2, r1, item2, item1, next, anno2, anno1, true );
+	}
+	else if ( item2.has_retain_item_count() )
+	{
+		var len = Math.min(item1.retain_item_count, item2.retain_item_count);
+		r1.component.push( JSOT.DocOp.newRetainItemCount( len ) );
+		r2.component.push( JSOT.DocOp.newRetainItemCount( len ) );
+		if ( len < item1.retain_item_count )
+			item1.retain_item_count -= len;
+		else
+			next.next1 = true;
+		if ( len < item2.retain_item_count )
+			item2.retain_item_count -= len;
+		else
+			next.next2 = true;
+	}
+	else if ( item2.has_delete_element_start() || item2.has_delete_element_end() )
+	{
+		if ( item1.retain_item_count > 1 )
+			item1.retain_item_count -= 1;
+		else
+			next.next1 = true;
+		r2.component.push( item2 );
+		next.next2 = true;
+	}
+    else if ( item2.has_delete_characters() )
+	{
+		var len = Math.min(item1.retain_item_count, item2.delete_characters.length);
+		if ( len < item1.retain_item_count )
+			item1.retain_item_count -= len;
+		else
+			next.next1 = true;
+		r2.component.push( JSOT.DocOp.newDeleteCharacters( item2.delete_characters.substr(0, len ) ) );
+		if ( len < item2.delete_characters.length )
+			item2.delete_characters = item2.delete_characters.substring( len, item2.delete_characters.length );
+		else
+			next.next2 = true;
+	}
+	else if ( item2.has_replace_attributes() || item2.has_update_attribute() )
+	{
+		r1.components.push( JSOT.DocOp.newRetainItemCount(1) );
+		if ( item1.retain_item_count > 1 )
+			item1.retain_item_count -= 1;
+		else
+			next.next1 = true;
+		r2.component.push( item2 );
+		next.next2 = true;
+	}
+	else
+		throw "Unexpected case";
+};
+
+JSOT.DocOp.xformDeleteElementStart = function( r1, r2, item1, item2, next, anno1, anno2 )
+{
+	if ( item2.has_element_start() )
+	{
+		JSOT.DocOp.xformInsertElementStart( r2, r1, item2, item1, next, anno2, anno1);
+	}
+	else if ( item2.has_element_end() )
+	{
+		JSOT.DocOp.xformInsertElementEnd( r2, r1, item2, item1, next, anno2, anno1);
+	}
+	else if ( item2.has_characters() )
+	{
+		JSOT.DocOp.xformInsertChars( r2, r1, item2, item1, next, anno2, anno1);
+	}
+	else if ( item2.has_annotation_boundary() )
+	{
+		JSOT.DocOp.xformAnnotationBoundary( r2, r1, item2, item1, next, anno2, anno1, true );
+	}
+	else if ( item2.has_retain_item_count() )
+	{
+		r1.component.push( item1 );
+		next.next1 = true;
+		if ( item2.retain_item_count > 1 )
+			item2.retain_item_count -= 1;
+		else
+			next.next2 = true;		
+	}
+	else if ( item2.has_delete_start() )
+	{
+		next.next1 = true;
+		next.next2 = true;
+	}
+	else if ( item2.has_replace_attributes() || item2.has_update_attributes() )
+	{
+		r1.component.push( item1 );
+		next.next1 = true;
+		next.next2 = true;
+	}
+	else
+		throw "Unexpected case";
 };
