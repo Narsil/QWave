59b61f3980f6798981165003d53c1faf4de64168
Torben Weis <torben.weis@gmail.com>
2010-03-06 13:35:32 +0000

    Generate javascript in the directory webclient/javascript

--- refreshProtocols.sh
+++ refreshProtocols.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd tools/protojs && qmake && make
-cd ../../core/protocol && ../../tools/protojs/protojs *.proto --cpp_out=. --cppjson_out=.
-cd ../../waveserver/protocol && protoc messages.proto -I. -I../../core/protocol --cpp_out . && ../../tools/protojs/protojs webclient.proto -I. -I../../core/protocol --cpp_out=. --cppjson_out=.
+cd ../../core/protocol && ../../tools/protojs/protojs *.proto --cpp_out=. --cppjson_out=. --jsjson_out=../../webclient/javascript
+cd ../../waveserver/protocol && protoc messages.proto -I. -I../../core/protocol --cpp_out . && ../../tools/protojs/protojs webclient.proto -I. -I../../core/protocol --cpp_out=. --cppjson_out=. --jsjson_out=../../webclient/javascript
